645a33592ddd56cb8cd524861c03d499494898f0
priscillagoncalves <59652260+priscillagoncalves@users.noreply.github.com>
2020-03-17 01:24:51 -0300

    Add files via upload

---
+++ DTO/Tarefa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DTO
+{
+    public class Tarefa
+    {
+        public long Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public DateTime Data { get; set; }
+    }
+}
---
+++ DTO/TarefaCollection.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+namespace DTO
+{
+    public class TarefaCollection : List <Tarefa>
+    {       
+    }
+}
